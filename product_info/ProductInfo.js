04393ed054c2d949234685c30a4d6f97be80fbf5
Xheghun <ysenruf@gmail.com>
2023-05-10 16:35:56 +0100

    add product information

---
+++ product_info/ProductInfo.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native"
+
+function ProductInfo () {
+    return (
+    <View>
+        <Text>Hello, This is the Product Information screen</Text>
+    </View>
+    )
+}
+
+export default ProductInfo
---
+++ product_list/ProductListComponent.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native"
+
+function ProductList () {
+    return (
+    <View>
+        <Text>Hello, This is the product List screen</Text>
+    </View>
+    )
+}
+
+export default ProductList
